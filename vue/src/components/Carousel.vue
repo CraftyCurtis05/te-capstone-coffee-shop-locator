39bd8fe21517e38023f25da0543bc25c9fb36b91
Naomi Adams <naomijyc@yahoo.com>
2023-12-06 14:07:48 -0500

    1st push 12-16

--- vue/src/components/Carousel.vue
+++ vue/src/components/Carousel.vue
@@ -1,4 +1,4 @@
-<template>
+<!-- <template>
     <div class='carousel-view'>
       <transition-group
         class='carousel'
@@ -88,4 +88,98 @@ export default {
 .slide:last-of-type {
   opacity: 0;
 }
-</style>
+</style> -->
+
+<template>
+    <div class="carousel-container">
+      <div class="carousel">
+        <transition :name="transitionName">
+          <div :key="currentIndex" class="carousel-item">
+            <a :href="imageLinks[currentIndex]" target="_blank">
+            <img :src="images[currentIndex]" alt="Slide" />
+            </a>
+          </div>
+        </transition>
+      </div>
+      <button @click="prevSlide" class="prev-btn">Previous</button>
+      <button @click="nextSlide" class="next-btn">Next</button>
+    </div>
+  </template>
+  <script>
+  export default {
+    data() {
+      return {
+        images: [
+          'https://images.pexels.com/photos/3184286/pexels-photo-3184286.jpeg?auto=compress&cs=tinysrgb&w=800',
+          'https://images.pexels.com/photos/2159065/pexels-photo-2159065.jpeg?auto=compress&cs=tinysrgb&w=600',
+          'https://images.pexels.com/photos/1516983/pexels-photo-1516983.jpeg?auto=compress&cs=tinysrgb&w=600',
+          'https://images.pexels.com/photos/846080/pexels-photo-846080.jpeg?auto=compress&cs=tinysrgb&w=600'
+          // Add more image URLs as needed
+        ],
+        imageLinks: [
+        'http://www.google.com/',
+        'https://www.yahoo.com/?guccounter=1',
+        'https://www.bing.com/',
+      ],
+        currentIndex: 0,
+        transitionName: 'slide',
+      };
+    },
+    methods: {
+      prevSlide() {
+        this.transitionName = 'slide-prev';
+        this.currentIndex = (this.currentIndex - 1 + this.images.length) % this.images.length;
+      },
+      nextSlide() {
+        this.transitionName = 'slide-next';
+        this.currentIndex = (this.currentIndex + 1) % this.images.length;
+      },
+    },
+  };
+  </script>
+  
+  <style scoped>
+  .carousel-container {
+    max-width: 600px;
+    margin: auto;
+    position: relative;
+  }
+  .carousel {
+    overflow: hidden;
+  }
+  .carousel-item {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 200px; /* Adjust the height as needed */
+  }
+  .prev-btn,
+  .next-btn {
+    position: absolute;
+    top: 50%;
+    transform: translateY(-50%);
+    font-size: 18px;
+    cursor: pointer;
+    padding: 10px;
+  }
+  .prev-btn {
+    left: 10px;
+  }
+  .next-btn {
+    right: 10px;
+  }
+  .slide-prev-enter-active,
+  .slide-next-enter-active,
+  .slide-prev-leave-active,
+  .slide-next-leave-active {
+    transition: transform 0.5s;
+  }
+  .slide-prev-enter, .slide-next-leave-to {
+    transform: translateX(-100%);
+  }
+  .slide-next-enter, .slide-prev-leave-to {
+    transform: translateX(100%);
+  }
+  </style>
+
