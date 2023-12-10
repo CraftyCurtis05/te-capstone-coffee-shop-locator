e47f11ae47e79ad6f8addf6609888331661b73d4
David DeFosha <ddefosha@gmail.com>
2023-12-09 22:44:07 -0500

    Articles Page Tweaks

--- vue/src/views/ArticlesView.vue
+++ vue/src/views/ArticlesView.vue
@@ -1,26 +1,36 @@
 <template>
-    <div id="articles" class="text-center">
-        <h1>Articles</h1>  
-        <section>
-            <div><a href="https://riordanclinic.org/2018/03/13-health-benefits-of-coffee-based-on-science/?gclid=Cj0KCQiAsburBhCIARIsAExmsu76rAuS3rnEO2lznJSxaKKwz8erMeb-IEj_v6c1q9rGZAkSqsr1ehYaAqYOEALw_wcB"><img src="src\assets\health2.webp"></a></div>
-            <div><a href="https://riordanclinic.org/2018/03/13-health-benefits-of-coffee-based-on-science/?gclid=Cj0KCQiAsburBhCIARIsAExmsu76rAuS3rnEO2lznJSxaKKwz8erMeb-IEj_v6c1q9rGZAkSqsr1ehYaAqYOEALw_wcB">Health Benefits of Coffee</a></div>
-            <div><a href="https://www.medscape.com/viewarticle/994049?form=fpf"><img src="src\assets\brainBoosting.jpeg"></a></div>
-            <div><a href="https://www.medscape.com/viewarticle/994049?form=fpf">Coffee Brain Boosting Effect</a></div>
-            <div><a href="https://www.nytimes.com/2023/09/26/well/eat/coffee-benefits-caffeine-risks.html"><img src="src\assets\tooMuch.jpeg"></a></div>
-            <div><a href="https://www.nytimes.com/2023/09/26/well/eat/coffee-benefits-caffeine-risks.html">How much is too much Coffee?</a></div>
-            <div><a href="https://www.inc.com/geoffrey-james/scientists-just-discovered-best-time-of-day-to-drink-your-first-cup-of-coffee.html"><img src="src\assets\bestTime1.jpeg"></a></div>
-            <div><a href="https://www.inc.com/geoffrey-james/scientists-just-discovered-best-time-of-day-to-drink-your-first-cup-of-coffee.html">Best Time to Drink Coffee</a></div>
-        </section>
-    </div>
-  </template>
-
+  <h1>Articles</h1>
+    <section id="articles-pics">
+      <div class="articles-pics1"><a href="https://riordanclinic.org/2018/03/13-health-benefits-of-coffee-based-on-science/?gclid=Cj0KCQiAsburBhCIARIsAExmsu76rAuS3rnEO2lznJSxaKKwz8erMeb-IEj_v6c1q9rGZAkSqsr1ehYaAqYOEALw_wcB"><img src="src\assets\health2.webp"></a></div>
+      <div class="articles-pics2"><a href="https://www.medscape.com/viewarticle/994049?form=fpf"><img src="src\assets\brainBoosting.jpeg"></a></div>
+      <div class="articles-pics3"><a href="https://www.nytimes.com/2023/09/26/well/eat/coffee-benefits-caffeine-risks.html"><img src="src\assets\tooMuch.jpeg"></a></div>
+      <div class="articles-pics4"><a href="https://www.inc.com/geoffrey-james/scientists-just-discovered-best-time-of-day-to-drink-your-first-cup-of-coffee.html"><img src="src\assets\bestTime1.jpeg"></a></div>
+    </section>
+    <section id="articles-texts">
+      <div><a href="https://riordanclinic.org/2018/03/13-health-benefits-of-coffee-based-on-science/?gclid=Cj0KCQiAsburBhCIARIsAExmsu76rAuS3rnEO2lznJSxaKKwz8erMeb-IEj_v6c1q9rGZAkSqsr1ehYaAqYOEALw_wcB">Health Benefits of Coffee</a></div>
+      <div><a href="https://www.medscape.com/viewarticle/994049?form=fpf">Coffee Brain Boosting Effect</a></div>
+      <div><a href="https://www.nytimes.com/2023/09/26/well/eat/coffee-benefits-caffeine-risks.html">How much is too much Coffee?</a></div>
+      <div><a href="https://www.inc.com/geoffrey-james/scientists-just-discovered-best-time-of-day-to-drink-your-first-cup-of-coffee.html">Best Time to Drink Coffee</a></div>
+    </section>
+</template>
 <style>
 @import url('https://fonts.googleapis.com/css2?family=Lobster+Two&family=Ubuntu&display=swap');
-
 h1{
   font-family: 'Lobster Two', sans-serif;
   font-family: 'Ubuntu', sans-serif;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+section {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: space-around;
+}
+#articles-pics1 {
+  border: 10px solid white;
+  resize: both;
 }
 </style>
-
-  
