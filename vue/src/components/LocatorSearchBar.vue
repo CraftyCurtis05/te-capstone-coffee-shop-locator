db320269db4a5d9d00c7fb430d0ec1c365c57494
Jennifer Curtis <CraftyCurtis05@gmail.com>
2023-12-12 17:38:08 -0500

    Prepull

--- vue/src/components/LocatorSearchBar.vue
+++ vue/src/components/LocatorSearchBar.vue
@@ -19,3 +19,35 @@ export default {
     }
 };
 </script>
+
+<style scoped>
+  .location-container {
+    display: flex;
+    align-items: center;
+    background-color: #F9F1DC;
+    border-radius: 5px;
+    padding: 10px;
+    max-width: 800px;
+    margin: 0 auto;
+  }
+  .location-input {
+    flex: 1;
+    padding: 10px;
+    border: none;
+    border-radius: 5px;
+    outline: none;
+  }
+  .search-button {
+    background-color: rgb(223, 204, 145);
+    color: rgb(13, 12, 12);
+    border: none;
+    border-radius: 5px;
+    padding: 10px 20px;
+    margin-left: 10px;
+    cursor: pointer;
+  }
+  .search-button:hover {
+    background-color: rgb(105, 100, 100);
+    color: rgb(255, 252, 251);
+  }
+  </style>
--- vue/src/components/YelpBusinessSearch.vue
+++ vue/src/components/YelpBusinessSearch.vue
@@ -1,15 +1,17 @@
 <template>
-    <div>
-        <h1>List of local coffee shops near you:</h1>
-    </div>
-    <ul class="coffee-shop-list">
-        <div v-for="result in results" v-bind:key="result.id">
-            <p> {{ result.name }} </p>
-            <p> {{ result.location.display_address }} </p>
-            <p> {{ result.url }} </p>
-            <img v-bind:src="result.image_url" />
+    <div class="coffee-shop-container">
+        <h1 class="coffee-shop-title">List of local coffee shops near you:</h1>
+        <div class="coffee-shop-list">
+            <ul>
+                <div v-for="result in results" v-bind:key="result.id">
+                    <p class="shop-name"> {{ result.name }} </p>
+                    <p class="shop-address"> {{ result.location.display_address }} </p>
+                    <p> {{ result.url }} </p>
+                    <img class="shop-image" v-bind:src="result.image_url" />
+                </div>
+            </ul>
         </div>
-    </ul>
+    </div>        
 </template>
 
 <script>
@@ -40,15 +42,43 @@
     }
 </script>
 
-<style>
-@import url('https://fonts.googleapis.com/css2?family=Lobster+Two&family=Ubuntu&display=swap');
+<style scoped>
+  @import url('https://fonts.googleapis.com/css2?family=Lobster+Two&family=Ubuntu&display=swap');
 
-h1 {
-  font-family: 'Lobster Two', sans-serif;
-  font-family: 'Ubuntu', sans-serif;
-  font-weight: bolder;
-  font-size: 20px; /* Adjust the font size as needed */
-  text-align: center;
-  margin: 0;
-}
-</style>
+  h1.coffee-shop-title {
+    font-family: 'Lobster Two', sans-serif;
+    font-family: 'Ubuntu', sans-serif;
+    font-weight: bolder;
+    font-size: 20px;
+    /* Remove margin-top to center it vertically */
+    text-align: center !important;
+  }
+  .coffee-shop-container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    min-height: 100vh;
+  }
+  /*.coffee-shop-image-container {
+    text-align: center; /* Center the content horizontally
+  }*/
+  .shop-image {
+    width: 200px;
+    height: auto;
+    border: 3px solid #F4E2B5; /* Add a border around the image */
+    border-radius: 10px; /* Add border radius for a cute look */
+    transition: transform 0.2s ease-in-out; /* Add a smooth transform transition */
+  }
+  /* Apply a scale transformation when hovering over the image */
+  .shop-image:hover {
+    transform: scale(1.1);
+  }
+  /* Style the coffee shop names */
+  .shop-name {
+    font-family: 'Your Custom Font', sans-serif; /* Change to your desired font */
+    font-size: 18px; /* Adjust the font size as needed */
+    color: #333; /* Change to your desired text color */
+    margin-top: 10px; /* Add margin for spacing */
+  }
+  </style>
